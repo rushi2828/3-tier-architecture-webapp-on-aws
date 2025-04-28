66ebd08a80f07564724e80851d1c39c5d658c862
rushi2828 <rushi2828@gmail.com>
2025-04-28 09:41:56 +0530

    Add project files

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.ctuuy88m60cp.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Rushi123',
+    DB_DATABASE : 'webappdb'
+});
